e7d8e817b126b2543509ee8a041ecebb0623b8c6
Brad Williams <jb.wms89@gmail.com>
2020-06-14 12:43:43 -0400

    was able to log current time, now trying to identify current time within the array

--- Develop/script.js
+++ Develop/script.js
@@ -1,30 +1,15 @@
 $(document).ready(function () {
+
   var arrayOfHours = [
-    "0000",
-    "0100",
-    "0200",
-    "0300",
-    "0400",
-    "0500",
-    "0600",
-    "0700",
-    "0800",
-    "0900",
-    "1000",
-    "1100",
-    "1200",
-    "1300",
-    "1400",
-    "1500",
-    "1600",
-    "1700",
-    "1800",
-    "1900",
-    "2000",
-    "2100",
-    "2200",
-    "2300",
-    "2400",
+    "9 AM",
+    "10 AM",
+    "11 AM",
+    "12 PM",
+    "1 PM",
+    "2 PM",
+    "3 PM",
+    "4 PM",
+    "5 PM",
   ];
 
   for (var i = 0; i < arrayOfHours.length; i++) {
@@ -33,11 +18,20 @@ $(document).ready(function () {
     var timeTableRows = $("<div class='row form-group hour-rows'>");
     var hourEl = $("<div class='col-sm-1'>");
     var textEl = $("<textarea class='col-sm-10 time-sensitive form-control'>");
-    var saveEl = $("<button type='button' class='col-sm-1 btn btn-info i fas fa-lock'>");
+    var saveEl = $(
+      "<button type='button' class='col-sm-1 btn btn-info i fas fa-lock'>"
+    );
     hourEl.text(hourList);
     timeTableRows.append(hourEl).append(textEl).append(saveEl);
     $(".container").append(timeTableRows);
-  };
+  }
+
+//   if (moment().isSame() === currentTime) {
+//     $(".time-sensitive").attr("style", "background-color: red");
+//   }
+
+var currentTime = moment().format("hA");
+console.log(currentTime);
 
   var todaysDate = moment().format("dddd MMMM Do, YYYY");
   console.log(todaysDate);
