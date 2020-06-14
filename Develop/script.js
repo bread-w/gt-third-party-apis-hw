fe4a305fd7dec4fd71bfb53a6551029da772ec01
Brad Williams <jb.wms89@gmail.com>
2020-06-14 17:20:39 -0400

    going afk need break, still trrying to set up local storage, colors, etc.

--- Develop/script.js
+++ Develop/script.js
@@ -1,5 +1,5 @@
 $(document).ready(function () {
-  var arrayOfHours = [
+  var schedulerEl = [
     "9 AM",
     "10 AM",
     "11 AM",
@@ -13,26 +13,18 @@ $(document).ready(function () {
 
   var currentTime = moment().format("h A");
 
-  for (var i = 0; i < arrayOfHours.length; i++) {
-    console.log(arrayOfHours[i]);
-    var hourList = arrayOfHours[i];
+  for (var i = 0; i < schedulerEl.length; i++) {
+    console.log(schedulerEl[i]);
+    var agenda = schedulerEl[i];
     var timeTableRows = $("<div class='row form-group hour-rows'>");
-    var hourEl = $("<div class='col-sm-1'>");
-    var textEl = $("<textarea class='col-sm-10 time-sensitive form-control'>");
-    var saveEl = $(
+    var timeBlockEl = $("<div class='col-sm-1'>");
+    var inputEl = $("<textarea class='col-sm-10 time-sensitive form-control'>");
+    var lockEl = $(
       "<button type='button' class='col-sm-1 btn btn-info i fas fa-lock'>"
     );
-    hourEl.text(hourList);
-    timeTableRows.append(hourEl).append(textEl).append(saveEl);
+    timeBlockEl.text(agenda);
+    timeTableRows.append(timeBlockEl).append(inputEl).append(lockEl);
     $(".container").append(timeTableRows);
-
-    // if (moment(arrayOfHours[i]).isSame(currentTime) === true) {
-    //     $(".time-sensitive").attr("style", "background-color: red");
-    //   }
-    // var blockTime = arrayOfHours[i];
-    // if (blockTime.match(currentTime)) {
-    //   $(".time-sensitive").attr("style", "background-color: red");
-    // }
   }
 
   var todaysDate = moment().format("dddd MMMM Do, YYYY");
