c7aa50d64fa7b8b8da209e353cc3999b8e51a2a0
Brad Williams <jb.wms89@gmail.com>
2020-06-15 15:13:52 -0400

    local storage setting and getting into correct time blocks. Now working on color

--- Develop/script.js
+++ Develop/script.js
@@ -13,7 +13,7 @@ $(document).ready(function () {
 
   var todoList = [];
 
-  var currentTime = moment().format("h A");
+  var currentTime = moment().hour();
 
   for (var i = 0; i < scheduler.length; i++) {
     console.log(scheduler[i]);
@@ -22,12 +22,15 @@ $(document).ready(function () {
     var timeBlockEl = $("<div class='col-sm-1 hour'>");
     var inputEl = $("<textarea class='col-sm-10 form-control todo-storage'>");
     var submitEl = $(
-      "<button type='button' class='col-sm-1 btn btn-primary saveBtn i fas fa-lock store-todo'>"
+      "<button type='button' class='col-sm-1 btn btn-primary saveBtn i:hover fas fa-lock store-todo'>"
     );
+    // submitEl.val()= i;
     timeBlockEl.text(agendaEl);
+    inputEl.attr("id", agendaEl.replace(" ", ""));
     dailyScheduleEl.append(timeBlockEl).append(inputEl).append(submitEl);
     $(".container").append(dailyScheduleEl);
 
+    var timeBlockColor = agendaEl
     if (agendaEl.match(currentTime)) {
       $(inputEl).addClass("present");
     } else if (agendaEl < currentTime) {
@@ -46,12 +49,23 @@ $(document).ready(function () {
   });
 
   $(".store-todo").on("click", function (event) {
-    var setTodo = $(".todo-storage").val();
+    event.preventDefault();
+    var setTodo = $(this).siblings(".todo-storage").val();
+    var key = $(this).siblings(".todo-storage").attr("id");
+    localStorage.setItem(key, setTodo);
     todoList.push(setTodo);
     console.log(todoList);
-    event.preventDefault();
     if (todoList.length > 0) {
       console.log("You submitted the form.");
     }
   });
+  $("#9AM").val(localStorage.getItem("9AM"));
+  $("#10AM").val(localStorage.getItem("10AM"));
+  $("#11AM").val(localStorage.getItem("11AM"));
+  $("#12PM").val(localStorage.getItem("12PM"));
+  $("#1PM").val(localStorage.getItem("1PM"));
+  $("#2PM").val(localStorage.getItem("2PM"));
+  $("#3PM").val(localStorage.getItem("3PM"));
+  $("#4PM").val(localStorage.getItem("4PM"));
+  $("#5PM").val(localStorage.getItem("5PM"));
 });
